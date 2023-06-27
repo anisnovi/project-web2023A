1db4d9bba29c8e651770a08b0c447f76e1e4dc88
anisnovi <anisnovi811@gmail.com>
2023-06-28 02:36:01 +0700

    auth

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,15 @@ use Illuminate\Support\Facades\Auth;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/register', [AuthController::class, 'register'])->name('register');
+
+Route::group(['middleware' => 'guest'], function () { 
+Route::get('/register', [AuthController::class, 'register'])->name('register');//menampilkan
 Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
 Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+Route::post('/login', [AuthController::class, 'loginPost'])->name('login');//mengarahkan 
+});
 
+Route::group(['middleware' => 'auth'], function(){
 Route::get('/karyawan',[KaryawanController::class,'index']);
 Route::delete('/logout',[AuthController::class,'logout'])->name('logout');
 
@@ -33,4 +37,4 @@ Route::post('/karyawan/store',[KaryawanController::class,'store']);
 Route::get('/karyawan/{id}/update',[KaryawanController::class,'update']);
 Route::put('/karyawan/{id}',[KaryawanController::class,'edit']);
 Route::delete('/karyawan/{id}',[KaryawanController::class,'destroy']);
-
+});
